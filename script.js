eb60c1ee6a3bd3b67efd72188a2a02c34a86e910
Diego Mirabal <diegomirabalkth@gmail.com>
2026-04-30 22:03:53 -0400

    feat: ahora trabaja con funcion cubic simple, antes era una version simplificada de tcp

--- script.js
+++ script.js
@@ -1,6 +1,12 @@
 let cwnd, ssthresh;
 let time = 0;
 let data = [];
+let Wmax = 1;
+let K = 0;
+let lastLossTime = 0;
+
+const C = 0.4;
+const beta = 0.7;
 const canvas = document.getElementById("canvas");
 const ctx = canvas.getContext("2d");
 
@@ -10,28 +16,46 @@ function load() {
 
   time = 0;
   data = [];
+
+  Wmax = cwnd;
+  lastLossTime = 0;
+  K = Math.cbrt(Wmax * (1 - beta) / C);
 }
 function step() {
   time += 0.1;
 
   if (cwnd < ssthresh) {
-    cwnd += 1; 
+    cwnd += 1;
   } else {
-    cwnd += 0.1; 
+    let t = time - lastLossTime;
+    let cubic = C * Math.pow(t - K, 3) + Wmax;
+
+    if (cubic > cwnd) {
+      cwnd += 0.3;
+    } else {
+      cwnd += 0.05;
+    }
   }
 
   data.push({ t: time, cwnd });
   draw();
 }
 function loss() {
-  ssthresh = cwnd / 2;
-  cwnd = 1;
+  Wmax = cwnd;
+  ssthresh = cwnd * beta;
+  cwnd = ssthresh;
+
+  lastLossTime = time;
+  K = Math.cbrt(Wmax * (1 - beta) / C);
+
+  data.push({ t: time, cwnd });
+  draw();
 }
 function start() {
   load();
   let interval = setInterval(() => {
     step();
-    if (time > 30) clearInterval(interval);
+    if (time > 60) clearInterval(interval);
   }, 100);
 }
 function resetSim() {
@@ -45,8 +69,9 @@ function draw() {
   ctx.strokeStyle = "blue";
 
   data.forEach((p, i) => {
-    let x = p.t * 20;
+    let x = p.t * 10;
     let y = canvas.height - p.cwnd * 5;
+
     if (i === 0) ctx.moveTo(x, y);
     else ctx.lineTo(x, y);
   });
